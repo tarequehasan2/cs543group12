bcf48b4bf2183f868d91362d182393ff5bb1b05e
mdaniel <mdaniel@gmail.com>
2009-10-14 23:05:41 +0000

    * updated sayhi to read back its written file to exercise the read() syscall, too  * as you might suspect, it runs fine with the read and write impl

--- Nachos/src/nachos/test/sayhi.c
+++ Nachos/src/nachos/test/sayhi.c
@@ -3,7 +3,9 @@
 int main(int argc, char* argv[])
 {
   int fd;
+  int i;
   char buf[] = {'h','i','!','\0'};
+  char buf2[4];
 
   if (0 >= (fd = creat("hi.txt"))) {
     printf("error: unable to creat\n");
@@ -16,6 +18,24 @@ int main(int argc, char* argv[])
   if (0 > close(fd)) {
     printf("error: unable to close\n");
     return 1;
+  }
+  if (0 > (fd = open("hi.txt"))) {
+    printf("error: unable to open: %d\n", fd);
+    return 1;
+  }
+  if (sizeof(buf2) != read(fd, buf2, sizeof(buf2))) {
+    printf("error: unable to read\n");
+    return 1;
+  }
+  if (0 > close(fd)) {
+    printf("error: unable to close\n");
+    return 1;
+  }
+  for (i=0;i<sizeof(buf2);i++) {
+    if (buf[i] != buf2[i]) {
+      printf("mismatch at %d: %d <> %d\n", i, buf[i], buf2[i]);
+      return 1;
+    }
   }
   return 0;
 }
