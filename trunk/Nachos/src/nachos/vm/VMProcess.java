7d709aa1821cfe1d295346867bfd945ba909ad4e
jzellers <jzellers@eb22492a-abb1-11de-97f0-97815e7b2756>
2009-11-04 03:36:38 +0000

    Reverted

--- trunk/Nachos/src/nachos/vm/VMProcess.java
+++ trunk/Nachos/src/nachos/vm/VMProcess.java
@@ -1,8 +1,5 @@
 package nachos.vm;
 
-import java.util.Hashtable;
-import java.util.LinkedList;
-
 import nachos.machine.*;
 import nachos.threads.*;
 import nachos.userprog.*;
@@ -17,135 +14,8 @@ public class VMProcess extends UserProcess {
      */
     public VMProcess() {
 	super();
-	//jjt
-	Swap_File_Name = "swap";
     }
 
-    //jjt 
-    class PageTableKey {
-    	public int proccess_Id;
-    	public int Vpage_number;
-    	public boolean equals(Object obj) { 
-    		
-    		if (!(obj instanceof PageTableKey)) {
-    			return false;
-    		}
-    		PageTableKey proccess_Id_Vpage_number = (PageTableKey)obj  ;
-    		
-    		
-			return 
-			     (proccess_Id_Vpage_number.proccess_Id == this.proccess_Id)
-			     &&
-			     (proccess_Id_Vpage_number.Vpage_number == this.Vpage_number);
-		}
-    	
-    }
-    
-	static class PageTable {
-		static Lock lock = new Lock();
-		static Condition f_pages = new Condition(lock);
-		static Hashtable<PageTableKey, TranslationEntry> p_table = new Hashtable<PageTableKey, TranslationEntry>(Machine.processor().getNumPhysPages());
-		static LinkedList<Integer> free = new LinkedList<Integer>();
-		static LinkedList<TranslationEntry> recentlyUsed = new LinkedList<TranslationEntry>();
-		
-		static {
-			for(int i = 0; i < Machine.processor().getNumPhysPages(); i++) {
-				free.addLast(i);
-			}
-		}
-		
-		public static TranslationEntry get(PageTableKey Pid_Vpn) { 
-			VMProcess process = (VMProcess)VMKernel.currentProcess();
-			TranslationEntry entry = p_table.get(Pid_Vpn);
-			if(entry == null) {
-				
-				if(free.size() == 0) {
-					remove();
-				}
-				int index = free.removeFirst();
-				// TODO
-				entry = Swap.search(Pid_Vpn);
-				if(entry != null) {
-					entry.ppn = index;
-					//TODO
-					Swap.read_entry(entry);
-				} else {
-					
-					if(Pid_Vpn.Vpage_number < process.numPages) {
-						entry = new TranslationEntry(Pid_Vpn.Vpage_number, index, true, false, false, false);
-						if(Pid_Vpn.Vpage_number < (process.numPages - (1 + process.stackPages))) {
-							for(int s = 0; s < process.coff.getNumSections(); s++) {
-								CoffSection section = process.coff.getSection(s);
-								if(Pid_Vpn.Vpage_number < (section.getFirstVPN() + section.getLength())) {
-									entry.readOnly = section.isReadOnly();
-									section.loadPage(Pid_Vpn.Vpage_number - section.getFirstVPN(), index);
-									break;
-								}
-							}
-						} else {
-							
-							java.util.Arrays.fill(Machine.processor().getMemory(), index * pageSize, (index + 1) * pageSize, (byte) 0); 
-						}
-					} else {
-						process.handleSyscall(0, process.getPid(), 0, 0, 0);
-					}
-				}
-				p_table.put(Pid_Vpn, entry);
-			} else {
-				recentlyUsed.remove(entry);
-			}
-			recentlyUsed.addFirst(entry);
-			return entry;
-		}
-		
-		
-		public static void remove(PageTableKey Pid_Vpn, boolean swapOut) {
-			TranslationEntry entry = p_table.remove(Pid_Vpn);
-			if(entry != null) {
-				if(swapOut) {
-					// TODO 
-					Swap.write_entry(entry);
-				}
-				//TODO remove from TLB
-				free.add(entry.ppn);
-				recentlyUsed.remove(entry);
-			}
-		}
-		
-		static void remove() {
-			while(recentlyUsed.size() == 0) {
-				PageTable.f_pages.sleep();
-			}
-			VMProcess process = (VMProcess)VMKernel.currentProcess();
-			PageTableKey eliminate = process.new PageTableKey();
-			eliminate.proccess_Id = process.getPid();
-			eliminate.Vpage_number = recentlyUsed.getLast().vpn;
-			remove(eliminate, true);
-		}
-	}
-	
-   //TODO 
-	static class Swap {
-		
-		public static TranslationEntry search(PageTableKey Pid_Vpn) {
-			return null;
-		}
-		//TODO
-		public static void read_entry(TranslationEntry entry) {
-			
-		}
-		//TODO
-		public static void write_entry(TranslationEntry entry) {
-			
-		}
-		//TODO
-		public static void remove_entry(PageTableKey Pid_Vpn) {
-			
-		}
-				
-	}
-	
-    //jjt  
     /**
      * Save the state of this process in preparation for a context switch.
      * Called by <tt>UThread.saveState()</tt>.
@@ -196,9 +66,7 @@ public class VMProcess extends UserProcess {
 	    break;
 	}
     }
-	// jjt
-    public static String Swap_File_Name;
-    
+	
     private static final int pageSize = Processor.pageSize;
     private static final char dbgProcess = 'a';
     private static final char dbgVM = 'v';
