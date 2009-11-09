0affe3ec3f382dcf8d5a5955de78b8309ba7ff46
mdaniel@gmail.com <mdaniel@gmail.com@eb22492a-abb1-11de-97f0-97815e7b2756>
2009-11-09 01:22:36 +0000

    * extracted the functionality into IPT  * SATE += javadoc  * VMProcess -= the IPT functionality  * override UserProcess::getTranslationEntryForVirtualPage so we don't have to copy-and-paste writeVirtualMemory

--- trunk/Nachos/src/nachos/vm/VMProcess.java
+++ trunk/Nachos/src/nachos/vm/VMProcess.java
@@ -1,11 +1,5 @@
 package nachos.vm;
 
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
-
-import nachos.machine.CoffSection;
-import nachos.machine.Kernel;
 import nachos.machine.Lib;
 import nachos.machine.Machine;
 import nachos.machine.Processor;
@@ -28,14 +22,16 @@ public class VMProcess extends UserProcess
         debug("saveState()");
         debug("TLB: saving state and invalidating");
         boolean intStatus = Machine.interrupt().disable();
-        _tlbLock.acquire();
-    	for (int i=0; i<Machine.processor().getTLBSize(); i++){
-    		state[i] = Machine.processor().readTLBEntry(i);
+        tlbLock.acquire();
+        final Processor proc = Machine.processor();
+        final int tlbSize = proc.getTLBSize();
+        for (int i=0; i < tlbSize; i++){
+    		state[i] = proc.readTLBEntry(i);
     		TranslationEntry translationEntry = new TranslationEntry(state[i]);
     		translationEntry.valid = false;
-            Machine.processor().writeTLBEntry(i,translationEntry);
+            proc.writeTLBEntry(i,translationEntry);
     	}
-        _tlbLock.release();
+        tlbLock.release();
         Machine.interrupt().setStatus(intStatus);
     }
 
@@ -48,30 +44,16 @@ public class VMProcess extends UserProcess
         debug("restoreState()");
         debug("TLB:restoring");
         boolean intStatus = Machine.interrupt().disable();
-        _tlbLock.acquire();
-    	for (int i=0; i<Machine.processor().getTLBSize(); i++){
-    		if (state[i] != null){
-    		Machine.processor().writeTLBEntry(i, state[i]);
+        tlbLock.acquire();
+        final Processor proc = Machine.processor();
+        for (int i=0; i < proc.getTLBSize(); i++){
+    		if (state[i] != null) {
+    		    proc.writeTLBEntry(i, state[i]);
     		}
     	}
-        _tlbLock.release();
+        tlbLock.release();
         Machine.interrupt().setStatus(intStatus);
         debug("TLB: restored");
-    }
-
-    /**
-     * Constructs a new {@link TranslationEntry} and initializes it to be
-     * not valid, read-only, not used, not dirty with vpn and ppn of -1.
-     * @return a freshly initialized TranslationEntry.
-     */
-    private TranslationEntry newTranslationEntry() {
-        int vpn = -1;
-        int ppn = -1;
-        boolean valid = false;
-        boolean readOnly = true;
-        boolean used = false;
-        boolean dirty = false;
-        return new TranslationEntry(vpn, ppn, valid, readOnly, used, dirty);
     }
 
     /**
@@ -83,106 +65,16 @@ public class VMProcess extends UserProcess
     @Override
     protected boolean loadSections() {
         debug("loadSections()");
-
-        final VMKernel kernel = (VMKernel)Kernel.kernel;
-        
         boolean result;
-
-        Map<Integer, TranslationEntry> entryMap = getEntryMap();
-        int sections = coff.getNumSections();
-        for (int i = 0; i < sections; i++) {
-            CoffSection section = coff.getSection(i);
-            debug( "\tinitializing " + section.getName()
-                  + " section (" + section.getLength() + " pages)");
-            int firstVpn = section.getFirstVPN();
-            int len = section.getLength();
-            for (int s = 0; s < len; s++) {
-                int myVpn = firstVpn + s;
-                /// TODO: this is where lazy loading takes place; q.v. stack frames below
-                final int[] pages = kernel.malloc(1);
-                final int ppn;
-                if (null == pages || 0 == pages.length) {
-                    boolean intStatus = Machine.interrupt().disable();
-                    memoryLock.acquire();
-                    int luckyPage = findRollOutPage();
-                    SwapFile.rollOut(getPid(), luckyPage);
-                    invalidateEntryPageForPpn(luckyPage);
-                    memoryLock.release();
-                    Machine.interrupt().setStatus(intStatus);
-                    ppn = luckyPage;
-                } else {
-                    ppn = pages[0];
-                }
-                TranslationEntry entry = newTranslationEntry();
-                entry.readOnly = section.isReadOnly();
-                entry.valid = true;
-                entry.vpn = myVpn;
-                entry.ppn = ppn;
-                storeTLBEntry(entry);
-                section.loadPage(s, ppn);
-            }
-        }
-        int coffPages = entryMap.size();
-        int stackPages = super.numPages - coffPages;
-        for (int i = 0; i < stackPages; i++) {
-            final int stackVpn = coffPages + i;
-            TranslationEntry entry = newTranslationEntry();
-            entry.vpn = stackVpn;
-            // purposefully don't set ppn for stack pages
-            entry.ppn = -1;
-            entry.readOnly = false;
-            entry.valid = true;
-            entryMap.put(stackVpn, entry);
-        }
-        debug("loadSections entryMap := "+entryMap);
+        InvertedPageTable.addCoff(getPid(), coff, stackPages);
         result = true;
         debug("loadSections() <- "+result);
         return result;
     }
 
-    private void invalidateEntryPageForPpn(int ppn) {
-        TranslationEntry item = null;
-        for (TranslationEntry entry : getEntryMap().values()) {
-            if (entry.ppn == ppn) {
-                item = entry;
-                break;
-            }
-        }
-        if (null != item) {
-            storeSwappedEntry(item);
-            getEntryMap().remove(item.vpn);
-        }
-    }
-
-    private void storeSwappedEntry(TranslationEntry item) {
-        Collection<TranslationEntry> entries = SwappedPages.get(getPid());
-        if (null == entries) {
-            entries = new java.util.ArrayList<TranslationEntry>();
-            SwappedPages.put(getPid(), entries);
-        }
-        entries.add(item);
-    }
-
-    protected int findRollOutPage() {
-        int result;
-        java.util.List<TranslationEntry> pool = new java.util.LinkedList<TranslationEntry>();
-        for (Map<Integer,TranslationEntry> pages : MyTLBMap.values()) {
-            for (TranslationEntry entry : pages.values()) {
-                if (entry.dirty) continue;
-                if (!entry.valid) continue;
-                if (entry.used) continue;
-                if (-1 == entry.ppn) continue;
-                pool.add(entry);
-            }
-        }
-        int lottery = new java.util.Random().nextInt(pool.size());
-        result = pool.get(lottery).ppn;
-        debug("Lottery found "+result);
-        return result;
-    }
-
     @Override
     protected void allocPageTable() {
+        // we override this just to keep the superclass from allocing a pageTable
         debug("allocPageTable()");
     }
 
@@ -192,7 +84,7 @@ public class VMProcess extends UserProcess
     @Override
     protected void unloadSections() {
         debug("unloadSections()");
-    	VMKernel.free(this.getPid());
+        SwapFile.free( InvertedPageTable.findAllSwapPagesByPid(getPid()) );
     }
 
     @Override
@@ -200,6 +92,8 @@ public class VMProcess extends UserProcess
         boolean intStatus = Machine.interrupt().disable();
         memoryLock.acquire();
         int result;
+        int vpn = Processor.pageFromAddress(vaddr);
+        InvertedPageTable.setVirtualUsed(getPid(), vpn);
         result = super.readVirtualMemory(vaddr, data, offset, length);
         memoryLock.release();
         Machine.interrupt().setStatus(intStatus);
@@ -211,145 +105,44 @@ public class VMProcess extends UserProcess
         int result;
         boolean intStatus = Machine.interrupt().disable();
         memoryLock.acquire();
-        result = doWriteVirtualMemory(vaddr, data, offset, length);
+        int vpn = Processor.pageFromAddress(vaddr);
+        InvertedPageTable.setVirtualWritten(getPid(), vpn);
+        result = super.writeVirtualMemory(vaddr, data, offset, length);
         memoryLock.release();
         Machine.interrupt().setStatus(intStatus);
         return result;
     }
 
-    private static final Lock memoryLock = new Lock();
-
-    protected int doWriteVirtualMemory(int vaddr, byte[] data, int offset, int length) {
-        Lib.assertTrue(offset >= 0 && length >= 0 && offset+length <= data.length);
-
-        int vpn = Processor.pageFromAddress(vaddr);
-        TranslationEntry pageEntry = getEntryMap().get(vpn);
-        if (null == pageEntry) {
-            if (swapContainsVPN(vpn)) {
-                int pages[] = ((VMKernel)Kernel.kernel).malloc(1);
-                if (null == pages || 0 == pages.length) {
-                    error("Egad, have to swap to get the swap back in");
-                    return 0;
-                }
-                SwapFile.rollIn(getPid(), pages[0]);
-                pageEntry = findEntryForVpn(vpn);
-                pageEntry.ppn = pages[0];
-                SwappedPages.remove(getPid());
-            }
+    @Override
+    protected TranslationEntry getTranslationEntryForVirtualPage(int vpn) {
+        final TranslationEntry entry = InvertedPageTable.getTranslationEntryForVirtualPage(getPid(), vpn);
+        if (null == entry) {
+            error("Unmapped page table entry for VPN: "+vpn);
+            return null; // kaboom!
         }
-        if (null == pageEntry) {
-            error("Attempting to write to an unmapped page");
-            // TODO: signal page fault? bus error?
-            return 0;
-        }
-
-        if (-1 == pageEntry.ppn) {
-            if (! mallocStackFrame(pageEntry)) {
-                return 0;
-            }
-        }
-
-        if (pageEntry.readOnly) {
-            error("attempting to write to read-only memory");
-            return 0;
-        }
-
-        int paddr = convertVaddrToPaddr(vaddr);
-        if (-1 == paddr) {
-            error("Unable to convert virtual ("+vaddr+") to physical");
-            return 0;
-        }
-
-        byte[] memory = Machine.processor().getMemory();
-
-        if (paddr < 0 || paddr >= memory.length) {
-            error("Attempt to write out of entire memory bounds");
-            return 0;
-        }
-
-        int amount = Math.min(length, memory.length-paddr);
-        System.arraycopy(data, offset, memory, paddr, amount);
-
-        return amount;
-    }
-
-    private TranslationEntry findEntryForVpn(int vpn) {
-        TranslationEntry result = null;
-        final Collection<TranslationEntry> entries = SwappedPages.get(getPid());
-        for (TranslationEntry entry : entries) {
-            if (entry.vpn == vpn) {
-                result = entry;
-                break;
-            }
-        }
-        return result;
-    }
-
-    protected boolean mallocStackFrame(TranslationEntry entry) {
-        if (-1 == entry.ppn) {
-            debug("StackFrame["+entry.vpn+"]: alloc()ing");
-            int[] pages = ((VMKernel) Kernel.kernel).malloc(1);
-            if (null == pages || 0 == pages.length) {
-                error("Unable to alloc stack frame for VPN("+entry.vpn+")");
-                return false;
-            }
-            entry.ppn = pages[0];
-            entry.valid = true;
-            entry.dirty = false;
-            entry.used = false;
-            storeTLBEntry(entry);
-        } else {
-            error("Requested a stack frame malloc() on a non-stack frame TE: "
-                    +entry);
-            return false;
-        }
-        return true;
-    }
-
-    /**
-     * Retrieves the EntryMap for the current process's PID, and ensures
-     * the result is never null (updating the original Map if necessary).
-     * @return a non-null Entry Map.
-     */
-    private Map<Integer, TranslationEntry> getEntryMap() {
-        Map<Integer, TranslationEntry> entryMap = MyTLBMap.get(getPid());
-        if (null == entryMap) {
-            entryMap = new HashMap<Integer, TranslationEntry>();
-            MyTLBMap.put(getPid(), entryMap);
-        }
-        return entryMap;
-    }
-
-    protected void storeTLBEntry(TranslationEntry entry) {
-        getEntryMap().put(entry.vpn, entry);
+        return entry;
     }
 
     @Override
     protected int convertVaddrToPaddr(int vaddr) {
-        int vPageNumber = Processor.pageFromAddress(vaddr);
-        int pageAddress = Processor.offsetFromAddress(vaddr);
-        final Map<Integer, TranslationEntry> entryMap = getEntryMap();
-        if (vPageNumber >= entryMap.size()) {
-            error("Requested a page number (" + vPageNumber
-                    + ") outside the page mapping (" + pageTable.length + " total)");
-            return -1;
-        }
-        TranslationEntry translationEntry = entryMap.get(vPageNumber);
-        if (null == translationEntry) {
-            // TODO: bus error? page fault?
-            error("Unmapped page table entry for VPN " + vPageNumber);
-            return -1;
-        }
-        // TODO: validity check?
-        int pPageNumber = translationEntry.ppn;
+		int vPageNumber = Processor.pageFromAddress(vaddr);
+		int pageAddress = Processor.offsetFromAddress(vaddr);
+		TranslationEntry translationEntry = getTranslationEntryForVirtualPage(vPageNumber);
+		if (null == translationEntry) {
+			// TODO: bus error? page fault?
+			error("Unmapped page table entry for VPN "+vPageNumber);
+			return -1;
+		}
+		// TODO: validity check?
+		int pPageNumber = translationEntry.ppn;
 
-        if (pageAddress < 0 || pageAddress >= Processor.pageSize) {
-            debug("bogus pageAddress: " + pageAddress+" outside "+Processor.pageSize);
-            return -1;
-        }
+		if (pageAddress < 0 || pageAddress >= Processor.pageSize) {
+			error("bogus pageAddress: "+pageAddress);
+		    return -1;
+		}
 
-        return Processor.makeAddress(pPageNumber, pageAddress);
-    }
+		return Processor.makeAddress(pPageNumber, pageAddress);
+	}
 
     /**
      * Handle a user exception. Called by
@@ -362,7 +155,7 @@ public class VMProcess extends UserProcess
     @Override
     public void handleException(int cause) {
         debug("handleException("+cause+")");
-	Processor processor = Machine.processor();
+	    Processor processor = Machine.processor();
 
 	switch (cause) {
         case Processor.exceptionTLBMiss: {
@@ -382,71 +175,15 @@ public class VMProcess extends UserProcess
         return "VMProcess[pid="+getPid()+"]";
     }
 
-    private static Lock _tlbLock = new Lock();
 
     protected void handleTLBMiss(int vaddr) {
         boolean intStatus = Machine.interrupt().disable();
-        _tlbLock.acquire();
         int page = Processor.pageFromAddress(vaddr);
-//        int offset = Processor.offsetFromAddress(vaddr);
-
         debug("vaddr("+Integer.toHexString(vaddr)+"):=pid="+getPid()+";vpn="+page);
-
-        final Processor proc = Machine.processor();
-
-        Map<Integer, TranslationEntry> entryMap = getEntryMap();
-        if (null != entryMap && entryMap.containsKey(page)) {
-            // just needs to be put back in the cache
-            TranslationEntry entry = entryMap.get(page);
-            // stack frame, needs to be alloced
-            if (-1 == entry.ppn) {
-                if (!mallocStackFrame(entry)) {
-                    error("Unable to malloc stack frame");
-                    _tlbLock.release();
-                    Machine.interrupt().setStatus(intStatus);
-                    return;
-                }
-            }
-            int frame = entry.ppn;
-            entry.valid = true;
-            debug("I've seen that page before, it's at "+frame);
-
-            int tlbSize = proc.getTLBSize();
-            int tlbEntryNum = -1;
-            for (int i = 0; i < tlbSize; i++) {
-                if (! proc.readTLBEntry(i).valid) {
-                    tlbEntryNum = i;
-                    break;
-                }
-            }
-            if (-1 == tlbEntryNum) {
-                tlbEntryNum = new java.util.Random().nextInt(tlbSize);
-            }
-            debug("ejecting "+tlbEntryNum
-                    +" old:="+proc.readTLBEntry(tlbEntryNum)+" new:="+entry);
-            debug("expect write at "
-                    +Integer.toHexString(
-                    Processor.makeAddress(entry.ppn, Processor.offsetFromAddress(vaddr))
-                    )
-            );
-            proc.writeTLBEntry(tlbEntryNum, entry);
-        } else if (swapContainsVPN(page)) {
-        } else {
-            error("VPN["+page+"] needs to be loaded for pid="+getPid());
+        if (!InvertedPageTable.handleTLBMiss(getPid(), page)) {
+            error("Unable to handle TLB miss");
         }
-        _tlbLock.release();
         Machine.interrupt().setStatus(intStatus);
-    }
-
-    private boolean swapContainsVPN(int page) {
-        for (java.util.Collection<TranslationEntry> entries : SwappedPages.values()) {
-            for (TranslationEntry entry : entries) {
-                if (entry.vpn == page) {
-                    return true;
-                }
-            }
-        }
-        return false;
     }
 
     private void error(String message) {
@@ -460,12 +197,6 @@ public class VMProcess extends UserProcess
 //    private static final int pageSize = Processor.pageSize;
 //    private static final char dbgProcess = 'a';
     private static final char dbgVM = 'v';
-    /**
-     * Maps <tt>(getPid()+","+virtualPage)</tt>
-     * to the physical frame which contains it.
-     */
-    private static Map<Integer, Map<Integer, TranslationEntry>>
-        MyTLBMap = new HashMap<Integer, Map<Integer, TranslationEntry>>();
-    private static Map<Integer, java.util.Collection<TranslationEntry>>
-        SwappedPages = new HashMap<Integer, java.util.Collection<TranslationEntry>>();
+    private static final Lock tlbLock = new Lock();
+    private static final Lock memoryLock = new Lock();
 }
