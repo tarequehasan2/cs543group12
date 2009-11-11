fde6ec95d9062a98de2edd1db3851f43e305dbec
jzellers <jzellers@eb22492a-abb1-11de-97f0-97815e7b2756>
2009-11-11 21:11:31 +0000

    A little more random

--- trunk/Nachos/src/nachos/vm/RandomAlgorithm.java
+++ trunk/Nachos/src/nachos/vm/RandomAlgorithm.java
@@ -2,6 +2,7 @@ package nachos.vm;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 
 import nachos.machine.Lib;
 import nachos.machine.Machine;
@@ -22,7 +23,7 @@ public final class RandomAlgorithm implements Algorithm {
 	public int findVictim() {
 		int victim = -1;
 		do {
-			victim = Lib.random(Machine.processor().getNumPhysPages());
+			victim = new Random().nextInt(Machine.processor().getNumPhysPages());
 		} while (InvertedPageTable.isPinned(victim) );
 		InvertedPageTable.pin(victim);
 		return victim;
