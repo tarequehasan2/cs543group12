852186ab82d7ac695209c157b1dfc6ba37b5303f
jzellers <jzellers@eb22492a-abb1-11de-97f0-97815e7b2756>
2009-12-02 23:03:29 +0000

    Need to make sure we are not sending packets to ourselves.

--- Nachos/src/nachos/network/SocketOpenFile.java
+++ Nachos/src/nachos/network/SocketOpenFile.java
@@ -1,5 +1,6 @@
 package nachos.network;
 
+import nachos.machine.Machine;
 import nachos.machine.OpenFile;
 
 public class SocketOpenFile extends OpenFile
@@ -45,7 +46,11 @@ public class SocketOpenFile extends OpenFile
      */
     @Override
     public int write(byte[] buf, int offset, int length) {
-        return _kernel.write(_key, buf, offset, length);
+    	if (_key.getSourceHost() != Machine.networkLink().getLinkAddress()){
+    		return _kernel.write(_key, buf, offset, length);
+    	}else{
+    		return _kernel.write(SocketKey.reverse(_key), buf, offset, length);
+    	}
     }
 
     /** Always returns -1, because I do not support seeked reads. */
